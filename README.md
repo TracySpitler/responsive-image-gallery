30fe5155413b7ccea25b6dfacf981feaa5305ddc
Tracy Spitler <tracyspitler@gmail.com>
2018-03-23 23:54:05 -0400

    Updated files. Made minor changes.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # responsive-image-gallery
-A responsive image gallery
+A responsive image gallery with 'travel' query.
 
 Live link: https://tracyspitler.github.io/responsive-image-gallery/
--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@
 
 const client_id = 'client_id=f852db6eb81775d1c6d04696094453c7228fa15b76122906fdfaf31ad4397c62';
 const API = 'https://api.unsplash.com/';
-let randomPhoto = API + 'search/photos/?page=3&query=travel&?w=400&h=400&fit=crop&' + client_id;
+let randomPhoto = API + 'search/photos/?page=3&query=travel&' + client_id;
 
 fetch(randomPhoto)
   .then(response => response.json())
@@ -19,7 +19,7 @@ fetch(randomPhoto)
       for (var i = 0; i < 9; i++) {
         post += '<li>';
         post += '<div class="container">';
-        post += '<span><img src="' + data.results[i].urls.regular + '" alt="' + data.results[i].description + '" class="image">';
+        post += '<span><img src="' + data.results[i].urls.regular + ' srcset="https://placehold.it/300.jpg?text=300px 300w, https://placehold.it/600.jpg?text=600px 600w, https://placehold.it/800.jpg?text=800px 800w" sizes="(max-width:300px) 100vw, (max-width: 600px) 33vw, 300px" alt=""' + data.results[i].description + '" class="image">';
         post += '<div class="overlay">';
         post += '<div class="text">';
         post += '<p><a href="' + data.results[i].user.portfolio_url + '"><img class="svg i-user" src="image-gallery-assets/user-icon.svg#user-icon"></a>' + data.results[i].user.name + '</p>';
